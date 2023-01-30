333e633a3514c9d051dc476dbf8a78bfc2b0dcac
Debraj Das <80595897+Debrajdas180@users.noreply.github.com>
2023-01-30 23:16:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(`Hello world`);  // for printing in console 
+ 
+const object2 = require("./second"); // for importing object from another js 
+console.log(object2) 
---
+++ second.js
@@ -0,0 +1,7 @@
+object = {
+    Name : "Debraj",
+    age : 22 ,
+    place : "kolkata",
+    role : "hw_dev"
+}
+  module.exports = object;   // exporting the object 
